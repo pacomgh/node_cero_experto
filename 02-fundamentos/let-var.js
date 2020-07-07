da604e7a2787b574a363b71c4a71367a8c123620
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-07 18:18:03 -0500

    add files to fundamentos & update nodemon

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,18 @@
+//let funciona en el ambito que fue declara clase, metodo, ciclo, etc
+//var en una variable global que puede cambiar su valor en cualquier momento
+
+/*let nombre = 'Wolverine';
+
+if(true){
+    let nombre = 'Maia';
+}
+
+console.log(nombre);*/
+
+let i = 'Hola mundo';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${ i }`);
+}
+
+console.log(i);
