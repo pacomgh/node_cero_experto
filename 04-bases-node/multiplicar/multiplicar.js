3904a6aba91c736e45514ce005486dd490b37434
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-13 16:56:39 -0500

    add import external files module on multiplicar

---
+++ 04-bases-node/multiplicar/multiplicar.js
@@ -0,0 +1,29 @@
+//importacion de filesystem
+const fs = require('fs');
+
+let crearArchivo = (base) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un número`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= 10; i++) {
+            data += `${ base } * ${ i } = ${ base*i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+
+            if (err) reject(err)
+            else resolve(`tabla-${base}.txt`)
+
+        });
+    })
+}
+
+module.exports = {
+    crearArchivo
+}
