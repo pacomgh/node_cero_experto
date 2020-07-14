5b9f8e9c37444e944c18ace8e7069b2dd4f595dd
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-14 16:42:35 -0500

    add yargs librarie and few commands

--- 04-bases-node/app.js
+++ 04-bases-node/app.js
@@ -1,19 +1,35 @@
+//require paquete
+const argv = require('yargs')
+    .command('listar', 'Imprime las tablas de multiplicar', {
+        base: {
+            demand: true, //base es un valor oblogatorio
+            alias: 'b' //define un alias para el argumento de la base
+        },
+        limite: {
+            alias: 'l',
+            default: 10 //ponemos un valor default a este argumento
+        }
+    })
+    .argv;
 //const { nombreImport } importa el import y lo destructura para poder usar sus prestaciones
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 
-let base = 'abc';
-/* let data = '';
+let argv2 = process.argv;
 
+//console.log(argv.base);
+console.log('El limite es', argv.limite);
+
+//let base = 'abc';
+/* let data = '';
 for (let i = 1; i <= 10; i++) {
     data += `${ base } * ${ i } = ${ base*i }\n`;
 }
-
 fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
     if (err) throw err;
     console.log(`El archivo tabla-${base} ha sido creado`);
 }); */
 
-crearArchivo(base)
+/* crearArchivo(base)
     .then(archivo => console.log(`Atchivo creado ${archivo} `))
-    .catch(e => console.log(e))
+    .catch(e => console.log(e)) */
