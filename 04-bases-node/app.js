6ad9ca962242e8a16664ae114547f19503206546
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-15 16:19:13 -0500

    add config yargs optimitation file

--- 04-bases-node/app.js
+++ 04-bases-node/app.js
@@ -1,24 +1,31 @@
 //require paquete
-const argv = require('yargs')
-    .command('listar', 'Imprime las tablas de multiplicar', {
-        base: {
-            demand: true, //base es un valor oblogatorio
-            alias: 'b' //define un alias para el argumento de la base
-        },
-        limite: {
-            alias: 'l',
-            default: 10 //ponemos un valor default a este argumento
-        }
-    })
-    .argv;
+const colors = require('colors');
+const argv = require('./config/yargs').argv;
 //const { nombreImport } importa el import y lo destructura para poder usar sus prestaciones
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 
-let argv2 = process.argv;
+//let argv2 = process.argv;
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        console.log("listar");
+        break;
+    case 'crear':
+        console.log('crear');
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado ${archivo} `))
+            .catch(e => console.log(e))
+        break;
+    default:
+        console.log('El comando no se reconoce');
+
+}
 
 //console.log(argv.base);
-console.log('El limite es', argv.limite);
+//console.log('El limite es', argv.limite);
 
 //let base = 'abc';
 /* let data = '';
@@ -29,7 +36,3 @@ fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
     if (err) throw err;
     console.log(`El archivo tabla-${base} ha sido creado`);
 }); */
-
-/* crearArchivo(base)
-    .then(archivo => console.log(`Atchivo creado ${archivo} `))
-    .catch(e => console.log(e)) */
--- 04-bases-node/multiplicar/multiplicar.js
+++ 04-bases-node/multiplicar/multiplicar.js
@@ -1,7 +1,13 @@
 //importacion de filesystem
 const fs = require('fs');
+const colors = require('colors');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite) => {
+    for (var i = 0; i <= limite; i++)
+        console.log(`${base} * ${i} = ${base*i}`);
+}
+
+let crearArchivo = (base, limite) => {
     return new Promise((resolve, reject) => {
 
         if (!Number(base)) {
@@ -11,19 +17,20 @@ let crearArchivo = (base) => {
 
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${ base } * ${ i } = ${ base*i }\n`;
         }
 
-        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
 
             if (err) reject(err)
-            else resolve(`tabla-${base}.txt`)
+            else resolve(`tabla-${base}-al-${limite}.txt`.green)
 
         });
     })
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
