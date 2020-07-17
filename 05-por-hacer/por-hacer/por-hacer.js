86c2df6fabb3c6aecc061b2d140ada8356f6a29a
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-17 16:20:48 -0500

    add por-hacer file, contains basic functions

---
+++ 05-por-hacer/por-hacer/por-hacer.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('./db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+
+    });
+}
+
+const crear = (descripcion) => {
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    }
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+}
+
+module.exports = {
+    crear,
+}
