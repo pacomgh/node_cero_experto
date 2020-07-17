3349775623b2f640e373ac97ce86d2a3f784ca45
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-17 16:22:21 -0500

    add logic

--- 05-por-hacer/app.js
+++ 05-por-hacer/app.js
@@ -1,11 +1,13 @@
 //const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
 
     case 'listar':
