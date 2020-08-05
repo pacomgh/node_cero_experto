bcc9596523efca1542a668b29f66ac785a6f1f60
Francisco Guzmán <paco.mgh@gmail.com>
2020-08-04 20:16:55 -0500

    fix

--- 06-clima-mundo/app.js
+++ 06-clima-mundo/app.js
@@ -14,9 +14,9 @@ const encodedUrl = encodeURI(argv.direccion);
 console.log(encodedUrl);
 
 const instance = axios.create({
-    baseURL: `http://api.openweathermap.org/data/2.5/forecast?q=${encodedUrl}&APPID=2e3bc34cd2dcdeb1dc883242e0b4d52f`,
+    baseURL: `http://api.openweathermap.org/data/2.5/forecast?q=${encodedUrl}&APPID=yourappid`,
     headers: {
-        'APPID': '2e3bc34cd2dcdeb1dc883242e0b4d52f'
+        'APPID': 'yourappid'
     }
 });
 
