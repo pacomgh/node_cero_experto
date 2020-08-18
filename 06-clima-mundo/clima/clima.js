206ac22640defad5f80e31dbed29480a1482e2ed
Francisco Guzmán <paco.mgh@gmail.com>
2020-08-18 17:53:49 -0500

    fix

--- 06-clima-mundo/clima/clima.js
+++ 06-clima-mundo/clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getClima = async(lat, lng) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=yourapid&units=metric`);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=appid&units=metric`);
     return resp.data.main.temp;
 }
 
