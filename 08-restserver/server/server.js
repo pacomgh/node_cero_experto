571dc14d9ff63e8116d98b0120a13feaa54c8832
Francisco Guzmán <paco.mgh@gmail.com>
2020-09-14 17:35:47 -0500

    add user routes

--- 08-restserver/server/server.js
+++ 08-restserver/server/server.js
@@ -1,8 +1,11 @@
 //al ser el primer archivo lo lee y ejecuta al principio, configura todo
-//su contenido
+//su contenido, este archivo debe ser simple
 require('./config/config');
 const express = require('express');
+const moongose = require('mongoose');
+
 const app = express();
+
 //body parser procesa la info y la serializa en un json(obtenemos del post)
 const bodyParser = require('body-parser');
 
@@ -15,42 +18,16 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-        res.json('get usuario')
-    })
-    //crear nuevos registros
-app.post('/usuario', function(req, res) {
-    //el body aparece cuando el body parser procesa cualquier payload
-    //de las peticiones
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        //cachamos el error y mandamos una respuesta entendible
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre en necesario"
-        });
-    } else {
-        res.json({
-            //obtenemos los datos que se estan enviando y los mostramos en postman
-            persona: body
-        });
-    }
+//importamos y usamos las rutas del usuario
+app.use(require('./routes/usuario'));
 
 
-});
-//actualmente se usa para actualizar informacion
-app.put('/usuario/:id', function(req, res) {
-    //obtenemos el parametro del id para actualizar
-    let id = req.params.id; //id del final es el parametro
-    //retorna lo que se mande en el url como id
-    res.json({
-        id
-    });
-});
-//actualmente se usa solo para inhabilitar usuarios
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+//hacemos la conexion a mongodb mediante el puero #####/BD
+moongose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    //definimos un callback si se hace o no la conexion
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE!');
 });
 
 app.listen(process.env.PORT, () => {
